03d0e7ccd468d3e259affa8fe4d8726d2b13fe04
kharratmarie <kharrat.mariam@enis.tn>
2025-01-14 13:58:04 +0100

    dependances ajoutee

--- ServiceApresVente/ServiceApresVente/Program.cs
+++ ServiceApresVente/ServiceApresVente/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ServiceApresVente.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,8 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+builder.Services.AddDbContextPool<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
