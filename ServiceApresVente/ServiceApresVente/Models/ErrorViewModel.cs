7266c12944d68df29db5ebdced79269873c38b23
kharratmarie <kharrat.mariam@enis.tn>
2025-01-14 13:02:38 +0100

    first commit

---
+++ ServiceApresVente/ServiceApresVente/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ServiceApresVente.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
