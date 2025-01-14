298853ab999ff980dceda905523d9b77c41548fb
kharratmarie <kharrat.mariam@enis.tn>
2025-01-14 15:38:19 +0100

    entitys

--- ServiceApresVente/ServiceApresVente/obj/Debug/net8.0/ServiceApresVente.AssemblyInfo.cs
+++ ServiceApresVente/ServiceApresVente/obj/Debug/net8.0/ServiceApresVente.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceApresVente")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e0a939113085cb53c287d70af7fd104d183585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a9a99a6ac7a1cbe6dae7f4a769e2d943bedccf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceApresVente")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceApresVente")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
