2de926942b1f327072d3a52a4461d474bc8a57c3
kharratmarie <kharrat.mariam@enis.tn>
2025-01-14 15:40:37 +0100

    commit.

--- ServiceApresVente/ServiceApresVente/Models/Repositories/InterventionRepository.cs
+++ ServiceApresVente/ServiceApresVente/Models/Repositories/InterventionRepository.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using ServiceApresVente.Context;
 using ServiceApresVente.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,9 @@ namespace ServiceApresVente.Repositories
 {
     public class InterventionRepository : IInterventionRepository
     {
-        private readonly ApplicationDbContext _context;
+        private readonly AppDbContext _context;
 
-        public InterventionRepository(ApplicationDbContext context)
+        public InterventionRepository(AppDbContext context)
         {
             _context = context;
         }
--- ServiceApresVente/ServiceApresVente/Models/Repositories/ReclamationRepository .cs
+++ ServiceApresVente/ServiceApresVente/Models/Repositories/ReclamationRepository .cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using ServiceApresVente.Context;
 using ServiceApresVente.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,9 @@ namespace ServiceApresVente.Repositories
 {
     public class ReclamationRepository : IReclamationRepository
     {
-        private readonly ApplicationDbContext _context;
+        private readonly AppDbContext _context;
 
-        public ReclamationRepository(ApplicationDbContext context)
+        public ReclamationRepository(AppDbContext context)
         {
             _context = context;
         }
